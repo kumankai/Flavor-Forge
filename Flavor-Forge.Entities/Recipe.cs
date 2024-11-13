457e35eb8dcdb99862b6015bbc15276187884108
Jacob Angga <anggaj@shernet.sheridancollege.ca>
2024-11-13 15:49:46 -0500

    Finished Controllers and models

---
+++ Flavor-Forge.Entities/Recipe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Flavor_Forge.Entities
+{
+    public class Recipe
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public List<string>? Ingredients { get; set; }
+    }
+}
--- Flavor-Forge.Operations/Controllers/RecipeController.cs
+++ Flavor-Forge.Operations/Controllers/RecipeController.cs
@@ -1,10 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor.Compilation;
 
 namespace Flavor_Forge.Operations.Controllers
 {
     public class RecipeController : Controller
     {
-        public IActionResult Index()
+
+        public IActionResult AddRecipes()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+        public IActionResult Search()
         {
             return View();
         }
