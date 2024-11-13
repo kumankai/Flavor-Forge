9d223b1ede8e524cec48b5cb859c1440414a6683
Jacob Angga <anggaj@shernet.sheridancollege.ca>
2024-11-12 20:33:32 -0500

    Initialize User model

---
+++ Flavor-Forge.Entities/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flavor_Forge.Entities
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter a name")]
+        [StringLength(16, ErrorMessage = "Name length 3-16", MinimumLength = 3)]
+        public string? Username { get; set; }
+        [Required(ErrorMessage = "Please enter a password")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8,}$",
+           ErrorMessage = "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character.")]
+        public string? Password { get; set; }
+    }
+}
